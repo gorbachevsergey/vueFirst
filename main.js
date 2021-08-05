de81ef15cdfa65694fac81a10cf0d167418e0ce2
Sergey <gorbachev.saa@gmail.com>
2021-08-05 16:04:53 +0700

    first Vue

--- main.js
+++ main.js
@@ -2,8 +2,34 @@
 const App = {
     data() {
         return {
-            counter : 10,
-            title : 'Счетчик'
+            title : 'Список заметок',
+            placeholderString : 'Введите название заметки',
+            inputValue: '',
+            notes: ['first'],
+        }
+    },
+    methods: {
+        addNewNote(){
+            if(this.inputValue.length !== 0){
+                this.notes.push(this.inputValue)
+                this.inputValue=''
+            }
+        },
+        toUpper(item){
+           return item.toUpperCase()
+        },
+        deleteNote(index){
+            this.notes.splice(index,1)
+        }
+    },
+    computed: {
+        doubleCountComputed(){
+            return this.notes.length * 2
+        }
+    },
+    watch:{
+        inputValue(value){
+            console.log(value)
         }
     }
 
